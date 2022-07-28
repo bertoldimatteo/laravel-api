bad8715b8a633be8dfb51d988ea3dbf056ec4129
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-07-28 15:42:28 +0200

    aggiunto prima parte grafica con vue + stampo nomi post

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <h1>Voglio piangere</h1>
+    <h1>BoolBlog</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        {{post.title}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
     name: 'App',
+    data() {
+      return {
+        posts: []
+      };
+    },
+    created() {
+      axios.get('http://localhost:8000/api/posts')
+       .then((response) => {
+        this.posts = response.data;
+       })
+       .catch((e) => {
+        console.log(e);
+       })
+    },
 }
 </script>
 
